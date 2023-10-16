1eb4f8747722025799f46240d872d07191035246
Yanne-said Bensafia <yanne-said.bensafia@epita.fr>
2023-10-16 12:48:29 +0200

    feat(header): added responsive header

--- src/app/pokemon-list/header/header.component.ts
+++ src/app/pokemon-list/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  toggled: boolean = false
 
+  toggleMenu() {
+    this.toggled = !this.toggled
+  }
 }
