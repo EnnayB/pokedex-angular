a8c8d1c903f5b8383db72a4df06eb39073b68bdb
Constance <constance.carle@epita.fr>
2023-11-10 14:42:14 +0100

    fixes after review

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,13 +1,25 @@
-import { Injectable } from '@angular/core'
-import {User, UserService} from "./users.service"
+import { Injectable } from '@angular/core';
+import { User, UserService } from './users.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private currentUser: User | undefined;
+
   constructor(private userService: UserService) {}
 
   authenticate(login: string, password: string): User | undefined {
-    return this.userService.getUsers().find(u => u.login === login && u.password === password)
+    const user = this.userService.getUsers().find((u) => u.login === login && u.password === password)
+    this.currentUser = user
+    return user
+  }
+
+  getCurrentUser(): User | undefined {
+    return this.currentUser
+  }
+
+  logout(): void {
+    this.currentUser = undefined
   }
 }
--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core'
 })
 export class UserService {
   private users: User[] = [
-    { id: 1, login: 'john.doe', password: '1234', favoritePokemons: ['pikachu', 'charmander'] },
-    { id: 2, login: 'bob.doe', password: '0000', favoritePokemons: ['bulbasaur', 'squirtle'] },
+    { id: 1, login: 'john.doe', password: '1234', isAdmin: true, favoritePokemons: ['pikachu', 'charmander'] },
+    { id: 2, login: 'bob.doe', password: '0000', isAdmin: false, favoritePokemons: ['bulbasaur', 'squirtle'] },
   ]
 
   getUsers(): User[] {
@@ -18,5 +18,6 @@ export interface User {
   id: number
   login: string
   password: string
+  isAdmin: boolean
   favoritePokemons: string[]
 }
