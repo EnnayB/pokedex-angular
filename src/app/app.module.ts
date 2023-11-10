98d5099eaa9c609d77aabea039d8929d586071f2
Constance <constance.carle@epita.fr>
2023-11-10 16:37:14 +0100

    usage of <app-type-tag>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { LoginPageComponent } from './login-page/login-page.component';
 import {FormsModule} from "@angular/forms";
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import {SharedModule} from "./shared/shared.module";
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, '../assets/i18n/', '.json')
@@ -32,7 +33,8 @@ export function createTranslateLoader(http: HttpClient) {
       }
     }),
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
