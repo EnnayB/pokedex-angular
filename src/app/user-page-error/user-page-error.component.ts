be5c27f2f26234ddb032839c38ded5779a7ae12a
Constance <constance.carle@epita.fr>
2023-11-12 12:35:01 +0100

    return to pokedex button on the user-page-error

--- src/app/user-page-error/user-page-error.component.ts
+++ src/app/user-page-error/user-page-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-page-error',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-page-error.component.css']
 })
 export class UserPageErrorComponent {
+    constructor(private router: Router) {}
 
+    navigateToPokedex(): void {
+        this.router.navigate(['/pokedex']);
+    }
 }
