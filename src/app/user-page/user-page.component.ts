c6dc408c4d1b70755c6dcfb7e823fff7b8247d19
Constance <constance.carle@epita.fr>
2023-11-12 11:02:06 +0100

    basic user page

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { User, UserService } from '../service/users.service'
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.css']
+  styleUrls: ['./user-page.component.css'],
 })
-export class UserPageComponent {
+export class UserPageComponent implements OnInit {
+  users: User[] = []
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loadUsers()
+  }
+
+  loadUsers(): void {
+    this.users = this.userService.getUsers()
+  }
+
+  deleteUser(userId: number): void {
+    this.userService.deleteUser(userId)
+    this.loadUsers()
+  }
+
+  addUser(): void {
+    const newUser: User = {
+      id: this.users.length + 1,
+      login: 'NewUser', // fixme
+      password: 'Password',
+      isAdmin: false,
+    };
+
+    this.userService.addUser(newUser);
+    this.loadUsers();
+  }
 }
